f9499b247ec437d76ed5cd1ec1a50e368b094e7a
Your Name <you@example.com>
2020-06-07 17:13:32 -0500

    color

---
+++ code/color_segmentation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib.image import imsave
+from sklearn.cluster import KMeans
+from PIL import Image
+
+
+def segmentation(image, n_color=32):
+    model = KMeans(n_clusters=n_color)
+    shape = image.shape
+
+    image = np.reshape(image, (-1, 3))
+
+    cluster_index = model.fit_predict(image)
+    cluster_centers = np.array(model.cluster_centers_, dtype=np.int)
+    result = np.array([cluster_centers[idx] for idx in cluster_index], dtype=np.uint8).reshape(shape)
+    imsave('../results/segmented/sea1.png', result)
+    return model, result
+
+
+if __name__ == '__main__':
+    img = cv2.imread('../dataset/sea1.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (512, 512))
+
+    _, result = segmentation(img, n_color=6)
+    # imsave('../results/segmented/sea1.png', result)
+
--- code/dead_leaves_model.py
+++ code/dead_leaves_model.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import cv2
+from color_segmentation import segmentation
+from matplotlib.image import imsave
 
 
-def compute_dead_leaves_model(img_size=500, sigma=3, shape='disk', num_iter=5000, rmin=0.01, rmax=1):
-
+def compute_dead_leaves_model(img_size=500, sigma=3, shape='disk', num_iter=5000, rmin=0.01, rmax=1, n_color=10):
     result = np.zeros((img_size, img_size)) + np.Inf  # This will be our image to work on
     x = np.linspace(0, 1, img_size)
     Y, X = np.meshgrid(x, x)
@@ -19,6 +21,10 @@ def compute_dead_leaves_model(img_size=500, sigma=3, shape='disk', num_iter=5000
     csum = np.cumsum(rad_dist)
     rad_dist = (csum - np.min(csum)) / (np.max(csum) - np.min(csum))  # map the radius distance to 0~1
 
+    ref_img = cv2.imread('../dataset/grass1.jpg')  # choose your reference image to color
+    ref_img = cv2.cvtColor(ref_img, cv2.COLOR_BGR2RGB)
+    model, _ = segmentation(ref_img, n_color=n_color)
+
     for _ in range(num_iter):
         r = np.random.rand(1)
         min_index = np.argmin(np.abs(r - rad_dist))
@@ -26,14 +32,15 @@ def compute_dead_leaves_model(img_size=500, sigma=3, shape='disk', num_iter=5000
         r = rad_list[min_index]
         x = np.random.rand(1)
         y = np.random.rand(1)
-        a = np.random.rand(1)
+        a = np.random.randint(0, n_color)
 
         if shape == 'disk':
             table = np.isinf(result) & (((X - x) ** 2 + (Y - y) ** 2) < r ** 2)
             result[table] = a
 
         elif shape == 'square':
-            table = np.isinf(result) & ((np.abs(X - x) + np.abs(Y - y)) < r)
+
+            table = np.isinf(result) & (np.abs(X - x) < r) & (np.abs(Y - y) < r)
             result[table] = a
 
         if np.inf not in result:
@@ -41,12 +48,19 @@ def compute_dead_leaves_model(img_size=500, sigma=3, shape='disk', num_iter=5000
 
     result = np.where(np.isinf(result), 0, result)
 
-    return result
+    return np.array(result, dtype=np.int), model
 
 
 if __name__ == '__main__':
-    image = compute_dead_leaves_model(img_size=500, sigma=3)
-    plt.imshow(image, cmap='gray')
-    plt.show()
+    cluster_index, model = compute_dead_leaves_model(img_size=512, sigma=5, shape='disk')
 
+    shape = cluster_index.shape
+    cluster_index = np.reshape(cluster_index, shape[0]*shape[1])
+
+    cluster_centers = np.array(model.cluster_centers_, dtype=np.int)
+    result = np.array([cluster_centers[idx] for idx in cluster_index], dtype=np.uint8).reshape((shape[0], shape[1], 3))
+
+    plt.imshow(result)
+    plt.show()
+    imsave('../results/dead_leaves/generated_grass1.png', result)
 
